a4ea32a8aaaac77bcfe21d7c3e5008c36ac654da
Jose luis Sánchez - Porro Godoy <jose.sanchezporro@mirada.tv>
2024-09-22 18:36:07 +0200

    ✨ Add new features, add use of Bézier curves to smooth strokes and multi-strokes to be able to make signatures that have several parts

--- Sources/SignatureView/SignatureView.swift
+++ Sources/SignatureView/SignatureView.swift
@@ -1,6 +1,3 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import SwiftUI
 import CoreGraphics
 import UIKit
@@ -9,11 +6,14 @@ private let placeholderText = "Introduce firma"
 private let maxHeight: CGFloat = 300
 private let lineWidth: CGFloat = 1
 
+/// A `SignatureView` is a SwiftUI view where users can draw their signature.
+/// - Parameters:
+///   - onSave: A closure that gets called when the signature is saved as a `UIImage`.
+///   - onCancel: A closure that gets called when the signature is cancelled.
 public struct SignatureView: View {
     public let onSave: (UIImage) -> Void
     public let onCancel: () -> Void
 
-    
     @State private var saveSignature = false
     @State private var color = Color.black
     
@@ -30,8 +30,7 @@ public struct SignatureView: View {
     
     public var body: some View {
         VStack {
-            SignatureDrawView(drawing: $drawing,
-                              color: $color)
+            SignatureDrawView(drawing: $drawing, color: $color)
             HStack {
                 Button("Hecho", action: extractImageAndHandle)
                 Spacer()
@@ -42,11 +41,13 @@ public struct SignatureView: View {
         }.padding()
     }
     
+    /// Extracts the drawn signature from the view as a `UIImage` and passes it to the `onSave` closure.
     private func extractImageAndHandle() {
         let image: UIImage
-        let path = drawing.cgPath
-        let maxX = drawing.points.map { $0.x }.max() ?? 0
+        let path = drawing.path.cgPath // Convertimos el Path a CGPath
+        let maxX = drawing.points.map { $0.x }.max() ?? 0 // Calculamos el máximo en el eje X
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: maxX, height: maxHeight))
+        
         let uiImage = renderer.image { ctx in
             ctx.cgContext.setStrokeColor(color.uiColor.cgColor)
             ctx.cgContext.setLineWidth(lineWidth)
@@ -54,8 +55,10 @@ public struct SignatureView: View {
             ctx.cgContext.addPath(path)
             ctx.cgContext.drawPath(using: .stroke)
         }
+        
         image = uiImage
         
+        // Guardamos la firma en un archivo si es necesario
         if saveSignature {
             if let data = image.pngData(),
                let docsDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
@@ -66,6 +69,7 @@ public struct SignatureView: View {
         onSave(image)
     }
     
+    /// Clears the drawing and resets the view.
     private func clear() {
         drawing = DrawingPath()
         image = UIImage()
@@ -74,186 +78,139 @@ public struct SignatureView: View {
     }
 }
 
-
+/// `FramePreferenceKey` is used to track the size of the drawing area.
 struct FramePreferenceKey: PreferenceKey {
     static let defaultValue: CGRect = .zero
 
-  static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
-    value = nextValue()
-  }
+    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
+        value = nextValue()
+    }
 }
 
+/// A `SignatureDrawView` handles the actual drawing process by capturing points from a `DragGesture`.
+/// - Parameters:
+///   - drawing: A binding to a `DrawingPath` where the points are stored.
+///   - color: The color of the drawing stroke.
 struct SignatureDrawView: View {
-  @Binding var drawing: DrawingPath
-  @Binding var color: Color
-  
-  @State private var drawingBounds: CGRect = .zero
-  @State private var lastPoint: CGPoint = .zero // Añadir una variable para almacenar el último punto
-    
+    @Binding var drawing: DrawingPath
+    @Binding var color: Color
+
+    @State private var drawingBounds: CGRect = .zero
+    @State private var lastPoint: CGPoint = .zero
+
     var body: some View {
-      ZStack {
-        Color.white
-          .background(GeometryReader { geometry in
-            Color.clear.preference(key: FramePreferenceKey.self,
-                                   value: geometry.frame(in: .local))
-          })
-          .onPreferenceChange(FramePreferenceKey.self) { bounds in
-            drawingBounds = bounds
-          }
-        if drawing.isEmpty {
-          Text("Firma aquí")
-            .foregroundColor(.gray)
-        } else {
-          DrawShape(drawingPath: drawing)
-            .stroke(lineWidth: 2)
-            .foregroundColor(color)
-        }
-      }
-      .frame(height: 300)
-      .gesture(DragGesture()
-        .onChanged( { value in
-          if drawingBounds.contains(value.location) {
-            let distance = hypot(value.location.x - lastPoint.x, value.location.y - lastPoint.y)
-            
-            // Solo añadimos el punto si la distancia es mayor que un umbral pequeño
-              if distance > 1.5 { // Ajusta este valor según lo que necesites
-              drawing.addPoint(value.location)
-              lastPoint = value.location // Actualizamos el último punto
+        ZStack {
+            Color.white
+                .background(GeometryReader { geometry in
+                    Color.clear.preference(key: FramePreferenceKey.self,
+                                           value: geometry.frame(in: .local))
+                })
+                .onPreferenceChange(FramePreferenceKey.self) { bounds in
+                    drawingBounds = bounds
+                }
+            if drawing.isEmpty {
+                Text(placeholderText)
+                    .foregroundColor(.gray)
+            } else {
+                DrawShape(drawingPath: drawing)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(color)
             }
-          } else {
-            drawing.addBreak()
-          }
-        }).onEnded( { value in
-          drawing.addBreak()
-          lastPoint = .zero // Restablecemos el último punto al finalizar
-        }))
-      .overlay(RoundedRectangle(cornerRadius: 7)
-                .stroke(Color( red: 110/255, green: 182/255, blue: 225/255, opacity: 1)))
-  }
+        }
+        .frame(height: 300)
+        .gesture(DragGesture()
+            .onChanged { value in
+                if drawingBounds.contains(value.location) {
+                    let distance = hypot(value.location.x - lastPoint.x, value.location.y - lastPoint.y)
+
+                    // Solo añadir punto si la distancia es significativa
+                    if distance > 1.5 {
+                        drawing.addPoint(value.location)
+                        lastPoint = value.location
+                    }
+                }
+            }
+            .onEnded { _ in
+                drawing.addBreak() // Añadir un break cuando se termina el gesto (se levanta el dedo)
+                lastPoint = .zero
+            }
+        )
+        .overlay(RoundedRectangle(cornerRadius: 7)
+            .stroke(Color(red: 110 / 255, green: 182 / 255, blue: 225 / 255, opacity: 1)))
+    }
 }
+
+/// `DrawingPath` stores the points and breaks of the signature.
 struct DrawingPath {
     private(set) var points = [CGPoint]()
     private var breaks = [Int]()
-    
+
     var isEmpty: Bool {
         points.isEmpty
     }
-    
+
+    /// Adds a new point to the drawing.
+    /// - Parameter point: The point to be added.
     mutating func addPoint(_ point: CGPoint) {
         points.append(point)
     }
-    
+
+    /// Adds a break in the drawing, marking the end of a stroke.
     mutating func addBreak() {
         breaks.append(points.count)
     }
-    
-    var cgPath: CGPath {
-        let path = CGMutablePath()
-        guard let firstPoint = points.first else { return path }
-        path.move(to: firstPoint)
-        for i in 1..<points.count {
-            if breaks.contains(i) {
-                path.move(to: points[i])
-            } else {
-                path.addLine(to: points[i])
-            }
 
-        }
-        return path
-    }
-    
+    /// Creates a SwiftUI `Path` from the list of points.
     var path: Path {
         var path = Path()
-        guard let firstPoint = points.first else { return path }
-        path.move(to: firstPoint)
+        guard points.count > 1 else { return path }
+
+        // Inicia la ruta en el primer punto
+        path.move(to: points[0])
+
         for i in 1..<points.count {
             if breaks.contains(i) {
-                path.move(to: points[i])
+                path.move(to: points[i]) // Romper la línea si se encuentra un "break"
             } else {
-                path.addLine(to: points[i])
+                // Usar curva de Bézier cuadrática para suavizar
+                let currentPoint = points[i]
+                let previousPoint = points[i - 1]
+                let midPoint = CGPoint(
+                    x: (currentPoint.x + previousPoint.x) / 2,
+                    y: (currentPoint.y + previousPoint.y) / 2
+                )
+                path.addQuadCurve(to: midPoint, control: previousPoint)
             }
-
         }
         return path
     }
+
+    /// Converts the SwiftUI `Path` to a `CGPath` to be used with CoreGraphics.
+    func cgPath() -> CGPath {
+        let cgPath = path.cgPath
+        return cgPath
+    }
 }
 
+/// `DrawShape` is responsible for drawing the smooth path in the view.
 struct DrawShape: Shape {
     let drawingPath: DrawingPath
-    
+
     func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        guard let firstPoint = drawingPath.points.first else {
-            return path
-        }
-        
-        path.move(to: firstPoint)
-        
-        for i in 1..<drawingPath.points.count {
-            let currentPoint = drawingPath.points[i]
-            let previousPoint = drawingPath.points[i - 1]
-            let midPoint = CGPoint(
-                x: (currentPoint.x + previousPoint.x) / 2,
-                y: (currentPoint.y + previousPoint.y) / 2
-            )
-            path.addQuadCurve(to: midPoint, control: previousPoint)
-        }
-        
-        return path
+        return drawingPath.path
     }
 }
-
-
-
-//
-//struct SignatureViewTest: View {
-//    @State private var image: UIImage? = nil
-//    @State private var showPopover = false
-//    @State private var showSignatureField = true
-//    var body: some View {
-//                VStack {
-//                    Button("Botón") {
-//                        showPopover.toggle()
-//                    }.sheet(isPresented: $showPopover) {
-//                        SignatureView(onSave: { image in
-//                            self.image = image
-//                            showPopover.toggle()
-//                        }, onCancel: {
-//                            showPopover.toggle()
-//                        }).frame(width: 500, height: 300, alignment: .center)
-//
-//                    }
-//
-//                    if image != nil {
-//                        Image(uiImage: image!)
-//                    }
-//        }
-//    }
-//}
-
-//struct SignatureView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        SignatureView { image in
-//           _ =  Image(uiImage: image)
-//        } onCancel: {
-//            
-//        }
-//        .previewDevice("iPad Pro (9.7-inch)")
-//        .previewLayout(.device)
-//
-//    }
-//}
 
 #Preview {
     SignatureView { image in
-        _ =  Image(uiImage: image)
+        _ = Image(uiImage: image)
     } onCancel: {
         
     }
 }
 
 extension Color {
+    /// Converts a SwiftUI `Color` to a `UIColor`.
     var uiColor: UIColor {
         if #available(iOS 14, *) {
             return UIColor(self)
@@ -262,7 +219,8 @@ extension Color {
             return UIColor(red: components.r, green: components.g, blue: components.b, alpha: components.a)
         }
     }
-    
+
+    /// Extracts the RGBA components from a SwiftUI `Color`.
     private var components: (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
         let scanner = Scanner(string: self.description.trimmingCharacters(in: CharacterSet.alphanumerics.inverted))
         var hexNumber: UInt64 = 0
@@ -277,3 +235,4 @@ extension Color {
         return (r, g, b, a)
     }
 }
+
